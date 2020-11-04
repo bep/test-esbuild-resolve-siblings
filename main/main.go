384252ec7e642c93d61ef7b9959430d400c114cb
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2020-11-04 16:09:47 +0100

    Make it work using OnLoad

--- main/main.go
+++ main/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -25,11 +27,22 @@ window.hello = hello;
 				func(args api.OnResolveArgs) (api.OnResolveResult, error) {
 					if args.Path == "mymod" {
 						return api.OnResolveResult{
-							Path: filepath.Join(wd, "..", "module/src/hello.js"),
+							Path:      filepath.Join(wd, "..", "module/src/hello.js"),
+							Namespace: "custom",
 						}, nil
 					}
 					return api.OnResolveResult{}, nil
 
+				})
+			build.OnLoad(api.OnLoadOptions{Filter: `.*`, Namespace: "custom"},
+				func(args api.OnLoadArgs) (api.OnLoadResult, error) {
+					b, _ := ioutil.ReadFile(args.Path)
+					c := string(b)
+
+					return api.OnLoadResult{
+						ResolveDir: wd,
+						Contents:   &c,
+					}, nil
 				})
 		},
 	}
@@ -51,4 +64,8 @@ window.hello = hello;
 	if res.Errors != nil {
 		log.Fatal(res.Errors[0].Text)
 	}
+
+	f := res.OutputFiles[0]
+
+	fmt.Println(f.Path, ":\n", string(f.Contents))
 }
